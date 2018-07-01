808dc3a1d6b3612d2e1826a4fcbbc8dc73069e3e
Balint Lorand <balint.lorand@gmail.com>
2018-07-02 00:31:58 +0200

    Live TV improvements - Added channel controll to TV Live app - Added clunky channel constant service for channel numbers greater than 100 - Added subscription to and manual updating of values - Maxed TV volume at 30

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ function webos3Accessory(log, config, api) {
     if (this.volumeControl == undefined) {
         this.volumeControl = true;
     }
+    this.channelControl = config['channelControl'];
+    if (this.channelControl == undefined) {
+        this.channelControl = true;
+    }
+    this.channelIdMap = {};
+    this.channelConstant = 0;
     this.pollingEnabled = config['pollingEnabled'];
     if (this.pollingEnabled == undefined) {
         this.pollingEnabled = false;
@@ -52,8 +58,37 @@ function webos3Accessory(log, config, api) {
         lgtv.subscribe('ssap://com.webos.applicationManager/getForegroundAppInfo', (err, res) => {
             if (res && res.appId) {
                 this.log.info('webOS - current appId: %s', res.appId);
+
+                this.setAppSwitchManually(null, true, res.appId)
             }
         });
+
+        lgtv.subscribe('ssap://audio/getVolume', (err, res) => {
+            if (res.volume) {
+                this.log.info('webOS - volume changed', res.volume);
+                this.setVolumeLevelManually(null, parseInt(res.volume))
+            } 
+
+            if (res.muted != undefined) {
+                this.log.info('webOS - mute changed', res.muted);
+                this.setMuteStateManually(null, !res.muted)
+            }
+        });
+
+        lgtv.subscribe('ssap://tv/getCurrentChannel', (err, res) => {
+            if (res.channelNumber) {
+                this.log.info('webOS - channel number changed', res.channelNumber);
+                this.setChannelManually(null, parseInt(res.channelNumber))
+            }
+        });
+
+        lgtv.request('ssap://tv/getChannelList', (err, res) => {
+            this.channelIdMap = res.channelList.reduce((dict, x) => {
+                dict[x.channelNumber] = x.channelId
+                return dict
+            }, {})
+		});
+
         this.updateAccessoryStatus();
     });
 
@@ -101,7 +136,8 @@ function webos3Accessory(log, config, api) {
     this.enabledServices.push(this.powerService);
     this.enabledServices.push(this.informationService);
 	
-	this.prepareVolumeService();
+    this.prepareVolumeService();
+    this.prepareChannelConstantService();
 	this.prepareAppSwitchService();
 
 }
@@ -130,6 +166,37 @@ webos3Accessory.prototype.prepareVolumeService = function () {
 	
 };
 
+webos3Accessory.prototype.prepareChannelConstantService = function () {
+	
+	if(!this.channelControl) {
+		return;
+	}
+	
+    this.channelConstantService = new Service.Lightbulb(this.name + " Channel Constant", "channelConstantService");
+	
+    this.channelConstantService
+        .getCharacteristic(Characteristic.On)
+        .on('get', (callback) => {
+            callback(null, this.channelConstant > 0)
+        })
+        .on('set', (state, callback) => {
+            this.channelConstant = state ? 1 : 0
+            callback(null, this.channelConstant)
+        }); 
+
+    this.channelConstantService
+        .addCharacteristic(new Characteristic.Brightness())
+        .on('get', (callback) => {
+            callback(null, this.channelConstant);
+        })
+        .on('set', (constant, callback) => {
+            this.channelConstant = constant
+            callback(null, this.channelConstant);
+        }); 
+		
+	this.enabledServices.push(this.channelConstantService);
+};
+
 webos3Accessory.prototype.prepareAppSwitchService = function () {
 	
 	if(this.appSwitch == undefined ||  this.appSwitch == null || this.appSwitch.length <= 0){
@@ -141,8 +208,12 @@ webos3Accessory.prototype.prepareAppSwitchService = function () {
 	if(isArray){
 		this.appSwitchService = new Array();
 		this.appSwitch.forEach((value, i) => {
-			this.appSwitch[i] = str = this.appSwitch[i].replace(/\s/g, '');
-			this.appSwitchService[i] = new Service.Switch(this.name + " App: " + value, "appSwitchService" + i);
+            this.appSwitch[i] = str = this.appSwitch[i].replace(/\s/g, '');
+            if (this.appSwitch[i] == 'com.webos.app.livetv') {
+                this.appSwitchService[i] = new Service.Lightbulb(this.name + " App: " + value, "appSwitchService" + i);
+            } else {
+                this.appSwitchService[i] = new Service.Switch(this.name + " App: " + value, "appSwitchService" + i);
+            }
 		});
 	}else{
 		this.appSwitchService = new Service.Switch(this.name + " App: " + this.appSwitch, "appSwitchService");
@@ -157,7 +228,13 @@ webos3Accessory.prototype.prepareAppSwitchService = function () {
 					})
 				.on('set', (state, callback) => {
 						this.setAppSwitchState(state, callback, this.appSwitch[i]);
-					}); 
+                    });
+            if (this.appSwitch[i] == 'com.webos.app.livetv') {
+                this.appSwitchService[i]
+                .addCharacteristic(new Characteristic.Brightness())
+                .on('get', this.getChannel.bind(this))
+                .on('set', this.setChannel.bind(this));
+            }
 		});
 	}else{
 		this.appSwitchService
@@ -185,6 +262,34 @@ webos3Accessory.prototype.setMuteStateManually = function (error, value) {
     if (this.volumeService) this.volumeService.getCharacteristic(Characteristic.On).updateValue(value);
 };
 
+webos3Accessory.prototype.setVolumeLevelManually = function (error, value) {
+    if (this.volumeService) this.volumeService.getCharacteristic(Characteristic.Brightness).updateValue(value);
+};
+
+webos3Accessory.prototype.setChannelManually = function (error, value) {
+    if (this.appSwitchService && this.channelConstantService) {
+        
+        let isArray = Array.isArray(this.appSwitch);
+        
+        if(isArray){
+            this.appSwitch.forEach((app, i) => {
+                this.appSwitch[i] = str = this.appSwitch[i].replace(/\s/g, '');
+                if (this.appSwitch[i] == 'com.webos.app.livetv') {
+                    const channel = value % 100
+                    const constant = (value - (value % 100)) / 100
+
+                    //this.log.info('webOS - channel:' + channel +' constant:' + constant);
+
+                    this.appSwitchService[i].getCharacteristic(Characteristic.Brightness).updateValue(channel);
+                    this.channelConstantService.getCharacteristic(Characteristic.Brightness).updateValue(constant);
+                    this.channelConstantService.getCharacteristic(Characteristic.On).updateValue(constant > 0);
+                    this.channelConstant = constant
+                }
+            });
+        }
+    }
+};
+
 webos3Accessory.prototype.setAppSwitchManually = function (error, value, appId) {
 	if(this.appSwitchService){
 		if(Array.isArray(this.appSwitch)){
@@ -208,14 +313,16 @@ webos3Accessory.prototype.setAppSwitchManually = function (error, value, appId) 
 };
 
 webos3Accessory.prototype.updateAccessoryStatus = function () {
-    if (this.volumeService) this.checkMuteState(this.setMuteStateManually.bind(this));
-	if (this.appSwitchService) this.checkForegroundApp(this.setAppSwitchManually.bind(this));
+    // if (this.volumeService) this.checkMuteState(this.setMuteStateManually.bind(this));
+	// if (this.appSwitchService) this.checkForegroundApp(this.setAppSwitchManually.bind(this));
 };
 
 webos3Accessory.prototype.pollCallback = function (error, status) {
     if (!status) {
         this.powerService.getCharacteristic(Characteristic.On).updateValue(status);
-        if (this.volumeService) this.volumeService.getCharacteristic(Characteristic.On).updateValue(status);
+        if (this.channelConstantService) this.channelConstantService.getCharacteristic(Characteristic.On).updateValue(status);
+        this.setMuteStateManually(null, false);
+        this.setAppSwitchManually(null, false, null);
     } else {
         this.powerService.getCharacteristic(Characteristic.On).updateValue(status);
     }
@@ -280,6 +387,29 @@ webos3Accessory.prototype.checkVolumeLevel = function (callback) {
             } else {
                 this.log.info('webOS - TV volume: ' + res.volume);
                 callback(null, parseInt(res.volume));
+            }
+        });
+    } else {
+        callback(null, false);
+    }
+};
+
+webos3Accessory.prototype.checkCurrentChannel = function (callback) {
+    if (this.connected) {
+        lgtv.request('ssap://tv/getCurrentChannel', (err, res) => {
+            if (!res || err) {
+                callback(new Error('webOS - TV channel - error while getting current channel'));
+            } else {
+                this.log.info('webOS - TV channel: ' + res.channelNumber);
+
+                const channelNumber = parseInt(res.channelNumber)
+                const channel = channelNumber % 100
+                const constant = (channelNumber - (channelNumber % 100)) / 100
+                this.channelConstant = constant
+
+                //this.log.info('webOS - channel:' + channel +' constant:' + constant);
+
+                callback(null, channel);
             }
         });
     } else {
@@ -378,8 +508,33 @@ webos3Accessory.prototype.getVolume = function (callback) {
 
 webos3Accessory.prototype.setVolume = function (level, callback) {
     if (this.connected) {
+        if (level > 30) {
+            level = 30
+        }
         lgtv.request('ssap://audio/setVolume', {volume: level});
         callback(null, level);
+    } else {
+        callback(new Error('webOS - is not connected'))
+    }
+};
+
+webos3Accessory.prototype.getChannel = function (callback) {
+    setTimeout(this.checkCurrentChannel.bind(this, callback), 50);
+};
+
+webos3Accessory.prototype.setChannel = function (level, callback) {
+    if (this.connected) {
+        this.log.info("channel const" + this.channelConstant)
+        const constant = this.channelConstant < 10 ? (this.channelConstant * 100) :
+            ((this.channelConstant - this.channelConstant%10) * 10)
+        const channelNumber = constant + level
+        const channelId = this.channelIdMap[channelNumber]
+        if (channelId != undefined) {
+            lgtv.request('ssap://tv/openChannel', {channelId: channelId, channelNumber: channelNumber});
+            callback(null, level);
+        } else {
+            this.checkCurrentChannel(callback);
+        }
     } else {
         callback(new Error('webOS - is not connected'))
     }
@@ -397,9 +552,10 @@ webos3Accessory.prototype.setAppSwitchState = function (state, callback, appId) 
     if (this.connected) {
         if (state) {
             lgtv.request('ssap://system.launcher/launch', {id: appId});
-			this.setAppSwitchManually(null, true, appId);
+            this.setAppSwitchManually(null, true, appId);
         } else {
             lgtv.request('ssap://system.launcher/launch', {id: "com.webos.app.livetv"});
+            this.setAppSwitchManually(null, true, "com.webos.app.livetv");
         }
         callback(null, state);
     } else {
